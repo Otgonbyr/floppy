bc72d911f359b0182fbd2bb6790d89423b36e2a7
Otgonbyr <otgonbayar0503@gmail.com>
2023-10-20 12:37:56 +0800

    floppy

--- floppy.js
+++ floppy.js
@@ -12,23 +12,46 @@ let position = {
     x:0,
     y:0
 }
+
+// let positionBlock = [0, 0, 0, 0, 0, 0]
+
 let positionBlock = {
     x:0,
 }
 
-function moveBlock() {
-    positionBlock.x = positionBlock.x + 100
-    block.style.transform = `translate(-${positionBlock.x}px, 0px)`
-}
 function createBlockelements(num) {
     for (let a = 0; a <= num; a++) {
-        block = document.getElementsByClassName("block")
-        blocktop = document.getElementsByClassName("block-top")
-        blockbtm = document.getElementsByClassName("block-bottom")
+        let block = document.getElementsByClassName("block")
+        let blocktop = document.getElementsByClassName("block-top")
+        let blockbtm = document.getElementsByClassName("block-bottom")
+        block.appendchild(blocktop)
+        block.appendchild(blockbtm)
+        container.appendchild(block)
     }
 }
 
-createBlockelements(5)
+createBlockelements(2)
+
+function moveBlocks() {
+    positionBlock.x = positionBlock.x + 100;
+    const blocks = document.getElementsByClassName("block");
+
+    // for (let a = 0; a < positionBlock.length; a++) {
+    //     if(position[a] > window.innerWidth){
+    //     position[a] = 0
+    //     blocks[a].remove()
+    //     if(positionBlock[positionBlock.length -1 ] > 20){
+    //         createBlockelements[1]
+    //         positionBlock.push(0)
+    //     }
+    // }
+    // }
+    for (let a = 0; a < blocks.length; a++) {
+    blocks[a].style.transform = `translate(-${positionBlock[a]}px, 0px)`;
+    }
+    }
+
+    // setInterval(moveBlocks, 200)
 
 document.addEventListener("keypress", function move(event) {
     if(event.keyCode === 119){
@@ -42,7 +65,7 @@ document.addEventListener("keypress", function move(event) {
     }
     else if(event.keyCode === 100){
         position.x = position.x + 50
-        // moveBlock()
+        moveBlocks()
     }
     bird[0].style.transform = `translate(${position.x}px, ${position.y}px)`
 })
